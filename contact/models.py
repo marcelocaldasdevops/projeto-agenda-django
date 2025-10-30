da7dc0526cc68a5b88b4802871e9f9954ad30b4f
marcelocaldasdevops <marcelocaldasdevops@gmail.com>
2025-10-30 08:29:32 -0400

    adicionando owner e contatos fakers

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -22,6 +23,7 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
